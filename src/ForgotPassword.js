8f956d1fa50a37b5baf29f6325f51d258e130639
Programmingchuks24 <chuksezeaku@gmail.com>
2024-04-10 01:35:40 +0100

    changes to forgotpassword

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react"
 import React from 'react'
 
 function ForgotPassword() {
-  return (
-    <div>
 
-        <p>Forgotten passowrd</p>
+    const [statusquo, setStatusquo] = useState("");
+
+
+    const statusChange = (e) => {
+
+        setStatusquo(e.target.value);
+
+        
+    }
+
+    useEffect(() => {
+            
+        console.log(statusquo);
+
+    }, [statusquo]);
+  return (
+    <div className="flex flex-col items-center mt-3 gap-3">
+
+        <p>Forgot Password</p>
+
+
+
+        <div className="w-[400px] h-[300px] border border-slate-600 ml-9 pt-20 flex justify-center"> 
+
+            <input className = "border border-slate-600 h-20 w-[20rem] text-center  shadow-md shadow-gray-700 rounded-3xl" type = "text" value = {statusquo} onChange = {statusChange} placeholder = "Enter number"/>
+        </div>
     </div>
   )
 }
