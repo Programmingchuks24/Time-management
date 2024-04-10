677f06aeaee8f7300275235b6cafa33a02c2cdcd
Programmingchuks24 <chuksezeaku@gmail.com>
2024-04-11 00:25:13 +0100

    Updated forgot password

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react"
 import React from 'react'
+import { ToastContainer, toast } from "react-toastify";
 
 function ForgotPassword() {
 
     const [statusquo, setStatusquo] = useState("");
+
+
+    const handleClick = () => {
+
+        toast.success
+    }
 
 
     const statusChange = (e) => {
@@ -19,16 +26,20 @@ function ForgotPassword() {
 
     }, [statusquo]);
   return (
-    <div className="flex flex-col items-center mt-3 gap-3">
+    <div className="flex flex-col items-center mt-20 gap-5">
 
-        <p>Forgot Password</p>
+        <p className="text-xl font-bold">Forgot Password</p>
 
 
 
-        <div className="w-[400px] h-[300px] border border-slate-600 ml-9 pt-20 flex justify-center"> 
+        <div className="w-[400px] h-[250px] border border-slate-600 ml-9 pt-1 flex flex-col justify-around items-center rounded-xl"> 
 
             <input className = "border border-slate-600 h-20 w-[20rem] text-center  shadow-md shadow-gray-700 rounded-3xl" type = "text" value = {statusquo} onChange = {statusChange} placeholder = "Enter number"/>
+
+            <button onClick={handleClick} className="border border-slate-600 h-16 w-[16rem] rounded-full" >Send Code</button>
         </div>
+
+        <ToastContainer/>
     </div>
   )
 }
