b93a75de8e9efb528a4484c5ba6243351790bdcb
Programmingchuks24 <chuksezeaku@gmail.com>
2024-04-11 20:31:38 +0100

    changes to the screen width

--- src/Login.js
+++ src/Login.js
@@ -64,8 +64,8 @@ function Login() {
         <div className = "flex flex-col gap-5 items-center">
             <h1 className = " italic text-3xl font-bold text-center">Login</h1>
 
-            <div className="border border-slate-800 w-[500px] h-[400px]
-            bg-gray-200 rounded-2xl text-center flex flex-col gap-4 pt-6 shadow-lg shadow-gray-500 sm:w-[400px] sm:ml-2 ">
+            <div className="border border-slate-800 w-[400px] h-[400px]
+            bg-gray-200 rounded-2xl text-center flex flex-col gap-4 pt-6 shadow-lg shadow-gray-500">
 
                 <p className="text-xl">Email address or Phone Number</p>
                 <input value={email} onChange={textChange}  className = "border border-slate-600 h-16 shadow-md shadow-gray-700 rounded-3xl" type="text"/>
