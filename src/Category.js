81866dc2dad0cdc78dbc39bd81a73feab781f994
Programmingchuks24 <chuksezeaku@gmail.com>
2024-04-11 01:25:29 +0100

    changes to category

--- src/Category.js
+++ src/Category.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+
 function Category() {
   return (
-    <div>
+    <div className="flex flex-col gap-3 items-center">
 
-        <p>category</p>
+      <h1>Category</h1>
+
+
+      <div className="border border-slate-600 h-[35rem] w-[25rem] flex flex-col justify-evenly items-center text-center">
+        <p className="border border-slate-600 w-[20rem] h-14 shadow-md shadow-gray-700 rounded-3xl pt-3">School</p>
+        <p className="border border-slate-600 w-[20rem] h-14 shadow-md shadow-gray-700 rounded-3xl pt-3">Work</p>
+        <p className="border border-slate-600 w-[20rem] h-14 shadow-md shadow-gray-700 rounded-3xl pt-3">Add New Category</p>
+      </div>
+      
     </div>
   )
 }
